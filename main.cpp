a54be11a028f2eab680620063dcd871ccda889a1
Dan Kersten <danielytics@users.noreply.github.com>
2022-06-06 17:01:18 +0100

    Add support for collision sensors

--- main.cpp
+++ main.cpp
@@ -9,26 +9,32 @@
 
 namespace filter {
     enum class Flags : physx::PxU32 {
-        Enabled = 0b0001,
+        Enabled    = 0b0001,
+        Collisions = 0b0010,
+        Sensor     = 0b0100,
     };
 
-    physx::PxU32& id (physx::PxFilterData& fd) {return fd.word0;}
+    physx::PxU32& id   (physx::PxFilterData& fd) {return fd.word0;}
     physx::PxU32& mask (physx::PxFilterData& fd) {return fd.word1;}
-    physx::PxU32 flags (physx::PxFilterData& fd) {return fd.word2;}
-    bool is_enabled (physx::PxFilterData& fd) {return flags(fd) & magic_enum::enum_integer(Flags::Enabled);}
+    physx::PxU32 flags (const physx::PxFilterData& fd) {return fd.word2;}
+    bool is_enabled    (const physx::PxFilterData& fd) {return flags(fd) & magic_enum::enum_integer(Flags::Enabled);}
+    bool is_collisions (const physx::PxFilterData& fd) {return flags(fd) & magic_enum::enum_integer(Flags::Collisions);}
+    bool is_sensor     (const physx::PxFilterData& fd) {return flags(fd) & magic_enum::enum_integer(Flags::Sensor);}
 
-    template <Flags... FilterFlags> void set_flags (physx::PxFilterData& fd) {
-        fd.word2 = (fd.word2 & 0xfffffff0) | (magic_enum::enum_integer(FilterFlags) | ...);
+    template <typename... FilterFlags> void set_flags (physx::PxFilterData& fd, FilterFlags... flags) {
+        fd.word2 = (fd.word2 & 0xfffffff0) | (magic_enum::enum_integer(flags) | ...);
+    }
+    template <typename... FilterFlags> void clear_flags (physx::PxFilterData& fd, FilterFlags... flags) {
+        fd.word2 = fd.word2 & (~(magic_enum::enum_integer(flags) | ...) | 0xfffffff0);
     }
 
-    void setup (physx::PxRigidActor* actor, physx::PxU32 filter_id, physx::PxU32 filter_mask)
+    template <typename... FilterFlags>
+    void setup (physx::PxRigidActor* actor, physx::PxU32 filter_id, physx::PxU32 filter_mask, FilterFlags... flags)
     {
         physx::PxFilterData filter_data;
         id(filter_data) = filter_id; // set own ID
         mask(filter_data) = filter_mask;  // set ID mask to filter pairs that trigger a contact callback
-
-        // Always enabled
-        set_flags<filter::Flags::Enabled>(filter_data);
+        set_flags(filter_data, flags...);
 
         const physx::PxU32 num_shapes = actor->getNbShapes();
         std::vector<physx::PxShape*> shapes(num_shapes);
@@ -37,21 +43,21 @@ namespace filter {
             shape->setSimulationFilterData(filter_data);
         }
     }
-    void setup (physx::PxShape* shape, physx::PxU32 filter_id, physx::PxU32 filter_mask)
+
+    template <typename... FilterFlags>
+    void setup (physx::PxShape* shape, physx::PxU32 filter_id, physx::PxU32 filter_mask, FilterFlags... flags)
     {
         physx::PxFilterData filter_data;
         id(filter_data) = filter_id; // set own ID
         mask(filter_data) = filter_mask;  // set ID mask to filter pairs that trigger a contact callback
-
-        // Always enabled
-        set_flags<filter::Flags::Enabled>(filter_data);
+        set_flags(filter_data, flags...);
 
         // Add filter data to shape
         shape->setSimulationFilterData(filter_data);
     }
 }
 
-physx::PxFilterFlags contactReportFilterShader(
+physx::PxFilterFlags contact_report_filter_shader(
     physx::PxFilterObjectAttributes attributes_0, physx::PxFilterData filter_data_0,
 	physx::PxFilterObjectAttributes attributes_1, physx::PxFilterData filter_data_1,
 	physx::PxPairFlags& pair_flags,
@@ -60,37 +66,33 @@ physx::PxFilterFlags contactReportFilterShader(
     PX_UNUSED(constant_block);
     PX_UNUSED(constant_block_size);
 
-    // let triggers through
-    if(physx::PxFilterObjectIsTrigger(attributes_0) || physx::PxFilterObjectIsTrigger(attributes_1)) {
+    // Let triggers through
+    if (physx::PxFilterObjectIsTrigger(attributes_0) || physx::PxFilterObjectIsTrigger(attributes_1)) {
         pair_flags = physx::PxPairFlag::eTRIGGER_DEFAULT;
         return physx::PxFilterFlag::eDEFAULT;
     }
 
-    // Disabled bodies don't collide
-    if (!filter::is_enabled(filter_data_0) || !filter::is_enabled(filter_data_1)) {
-        return physx::PxFilterFlag::eKILL;
-    }
-    // generate contacts for all that were not filtered above
-    pair_flags = physx::PxPairFlag::eCONTACT_DEFAULT;
-
-    // trigger the contact callback for pairs (A,B) where
-    // the filtermask of A contains the ID of B and vice versa.
-    if(filter::is_enabled(filter_data_0) &&
-        (filter::mask(filter_data_0) & filter::id(filter_data_1)) && (filter::mask(filter_data_1) & filter::id(filter_data_0))) {
-        pair_flags |= physx::PxPairFlag::eNOTIFY_TOUCH_FOUND;
-    }
-
-    return physx::PxFilterFlag::eDEFAULT;
-
-	// pairFlags = physx::PxPairFlag::eCONTACT_DEFAULT
-	// 		  | physx::PxPairFlag::eDETECT_CCD_CONTACT
-	// 		  | physx::PxPairFlag::eNOTIFY_TOUCH_CCD
-	// 		  |	physx::PxPairFlag::eNOTIFY_TOUCH_FOUND
-	// 		  | physx::PxPairFlag::eNOTIFY_CONTACT_POINTS
-	// 		  | physx::PxPairFlag::eCONTACT_EVENT_POSE;
-	// return physx::PxFilterFlag::eDEFAULT;
+    // Suppress collisions if either body is disabled
+    if (filter::is_enabled(filter_data_0) && filter::is_enabled(filter_data_1)) {
+        // If both are defined to interact with each other
+        if (filter::mask(filter_data_0) & filter::id(filter_data_1) && filter::mask(filter_data_1) & filter::id(filter_data_0)) {
+            bool collisions = filter::is_collisions(filter_data_0) && filter::is_collisions(filter_data_1);
+            bool sensors = filter::is_sensor(filter_data_0) || filter::is_sensor(filter_data_1);
+            if (collisions || sensors) {
+                pair_flags = physx::PxPairFlag::eDETECT_DISCRETE_CONTACT;
+                if (collisions) {
+                    pair_flags |= physx::PxPairFlag::eSOLVE_CONTACT;
+                }
+                if (sensors) {
+                    pair_flags |= physx::PxPairFlag::eNOTIFY_TOUCH_FOUND;
+                }
+                return physx::PxFilterFlag::eDEFAULT;
+            }
+        }
+    } 
+    // Not enabled, not masked to interact or no appropriate flags set
+    return physx::PxFilterFlag::eSUPPRESS;
 }
-
 
 struct EntityInfo {
     entt::entity entity;
@@ -113,6 +115,7 @@ public:
     bool onLoad (million::Setup* api)
     {
         debug("Loading");
+        m_stream = &api->createStream("physics"_hs);
         m_foundation = PxCreateFoundation(PX_PHYSICS_VERSION, m_default_allocator_callback, m_default_error_callback);
         if (!m_foundation) {
             return false;
@@ -142,14 +145,22 @@ public:
         m_dispatcher = physx::PxDefaultCpuDispatcherCreate(2); // TODO: Read from engine config
         scene_desc.cpuDispatcher = m_dispatcher;
         scene_desc.broadPhaseType = physx::PxBroadPhaseType::eABP;
-        scene_desc.filterShader	= contactReportFilterShader; //physx::PxDefaultSimulationFilterShader;
+        scene_desc.filterShader	= contact_report_filter_shader;
         scene_desc.simulationEventCallback = this;
         m_scene = m_physics->createScene(scene_desc);
         m_material = m_physics->createMaterial(0.5f, 0.5f, 0.5f);
 
         float half_extent = 0.2f;
-        m_shape = m_physics->createShape(physx::PxBoxGeometry(half_extent, half_extent, half_extent), *m_material);
-        filter::setup(m_shape, 1, 1);
+
+        m_collide_only_shape = m_physics->createShape(physx::PxBoxGeometry(half_extent, half_extent, half_extent), *m_material);
+        filter::setup(m_collide_only_shape, 0b01, 0b111, filter::Flags::Enabled, filter::Flags::Collisions);
+
+        m_collide_and_notify_shape = m_physics->createShape(physx::PxBoxGeometry(half_extent, half_extent, half_extent), *m_material);
+        filter::setup(m_collide_and_notify_shape, 0b10, 0b111, filter::Flags::Enabled, filter::Flags::Collisions, filter::Flags::Sensor);
+
+        m_notify_only_shape = m_physics->createShape(physx::PxBoxGeometry(half_extent, half_extent, half_extent), *m_material);
+        filter::setup(m_notify_only_shape, 0b100, 0b111, filter::Flags::Enabled, filter::Flags::Sensor);
+         
 // #ifdef DEBUG_BUILD
 //         physx::PxPvdSceneClient* pvd_client = context->m_scene->getScenePvdClient();
 //         if(pvd_client) {
@@ -191,11 +202,27 @@ public:
             // organizer.emplace<&PhysX::read_physics, components::core::physics::StaticBody, const components::core::physics::DynamicBody, const components::core::Position>(*this, "Read Physics");
             organizer.emplace<&PhysX::create_new_static_bodies, Static>(*this, "Create Static Bodies");
             organizer.emplace<&PhysX::create_new_dynamic_bodies, Dynamic>(*this, "Create Dynamic Bodies");
+            organizer.emplace<&PhysX::create_new_collision_sensors, Dynamic>(*this, "Create Collision Sensors");
             organizer.emplace<&PhysX::read_physics_objects, const components::core::physics::StaticBody, const components::core::physics::DynamicBody, const components::core::Position, Static, Dynamic>(*this, "Read Physics Bodies");
         }
         {
             auto& organizer = api->organizer(million::SystemStage::Update);
             organizer.emplace<&PhysX::write_dynamic_objects>(*this, "Write Dynamic Bodies");
+            organizer.emplace<&PhysX::notify_collisions>(*this, "Notify Collisions");
+        }
+    }
+
+    void create_new_collision_sensors (entt::registry& registry, entt::view<entt::get_t<const components::core::physics::CollisionSensor, const components::core::Position>, entt::exclude_t<Static, components::core::physics::StaticBody, components::core::physics::DynamicBody>> view)
+    {
+        for (const auto&& [entity, sensor, position] : view.each()) {
+            physx::PxTransform tx(physx::PxVec3(position.x, position.y, position.z));
+            physx::PxRigidStatic* px_body = m_physics->createRigidStatic(tx);
+            px_body->attachShape(*m_notify_only_shape);
+            EntityInfo* entity_info = new EntityInfo;
+            entity_info->entity = entity;
+            px_body->userData = reinterpret_cast<void*>(entity_info);
+            m_scene->addActor(*px_body); 
+            registry.emplace<Static>(entity, px_body);
         }
     }
 
@@ -204,7 +231,11 @@ public:
         for (const auto&& [entity, body, position] : view.each()) {
             physx::PxTransform tx(physx::PxVec3(position.x, position.y, position.z));
             physx::PxRigidStatic* px_body = m_physics->createRigidStatic(tx);
-            px_body->attachShape(*m_shape);
+            if (registry.any_of<components::core::physics::CollisionSensor>(entity)) {
+                px_body->attachShape(*m_collide_and_notify_shape);
+            } else {
+                px_body->attachShape(*m_collide_only_shape);
+            }
             EntityInfo* entity_info = new EntityInfo;
             entity_info->entity = entity;
             px_body->userData = reinterpret_cast<void*>(entity_info);
@@ -218,7 +249,11 @@ public:
         for (const auto&& [entity, body, position] : view.each()) {
             physx::PxTransform tx(physx::PxVec3(position.x, position.y, position.z));
             physx::PxRigidDynamic* px_body = m_physics->createRigidDynamic(tx);
-            px_body->attachShape(*m_shape);
+            if (registry.any_of<components::core::physics::CollisionSensor>(entity)) {
+                px_body->attachShape(*m_collide_and_notify_shape);
+            } else {
+                px_body->attachShape(*m_collide_only_shape);
+            }
             EntityInfo* entity_info = new EntityInfo;
             entity_info->entity = entity;
             px_body->userData = reinterpret_cast<void*>(entity_info);
@@ -255,6 +290,18 @@ public:
         });
     }
 
+    void notify_collisions (const entt::registry& registry, const million::api::Runtime& api)
+    {
+        auto& publisher = api.publisher();
+        for (auto& contact : m_contacts) {
+            auto sensor = registry.try_get<components::core::physics::CollisionSensor>(contact->entity);
+            if (sensor) {
+                publisher.post(contact->entity, sensor->on_collision);
+            }
+        }
+        m_contacts.clear();
+    }
+
 	void onConstraintBreak(physx::PxConstraintInfo* constraints, physx::PxU32 count) override
     {
 
@@ -282,17 +329,8 @@ public:
 
     void onContact(const physx::PxContactPairHeader& header, const physx::PxContactPair* pairs, physx::PxU32 num_pairs) override
     {
-        info("Contact!");
         m_contacts.emplace_back(reinterpret_cast<EntityInfo*>(header.actors[0]->userData));
         m_contacts.emplace_back(reinterpret_cast<EntityInfo*>(header.actors[1]->userData));
-    }
-
-    void onAfterFrame (million::Runtime* api)
-    {
-        for (auto& contact : m_contacts) {
-            info("Contacting: {}", api->findEntityName(contact->entity));
-        }
-        m_contacts.clear();
     }
 
 private:
@@ -305,11 +343,14 @@ private:
     physx::PxMaterial* m_material = nullptr;
     physx::PxPvd* m_pvd = nullptr;
 
-    physx::PxShape* m_shape = nullptr;
+    physx::PxShape* m_collide_only_shape = nullptr;
+    physx::PxShape* m_collide_and_notify_shape = nullptr;
+    physx::PxShape* m_notify_only_shape = nullptr;
 
     PhysicsShapeLoader* m_shape_loader = nullptr;
 
     std::vector<EntityInfo*> m_contacts;
+    million::events::Stream* m_stream;
 };
 
 MM_MODULE(PhysX)
